044bc3f35cd978a330ddd9c3f7e13e6c0d162eaa
Brunoake <126792752+Brunoake@users.noreply.github.com>
2023-12-12 08:40:25 -0300

    começando java script

---
+++ 2_operadores/js/scripts.js
@@ -0,0 +1,26 @@
+// number
+console.log(typeof 2);
+console.log(typeof 19.8);
+console.log(typeof -140);
+console.log(typeof 34);
+// aritmética
+
+console.log(2 + 4);
+
+console.log(10 - 4);
+
+console.log(150 * 12);
+
+console.log(140 / 3); 
+
+console.log(4 + (12 * 5));
+
+// special number
+
+console.log(typeof Infinity);
+
+console.log(typeof -Infinity);
+
+console.log(12 * NaN);
+
+console.log(typeof NaN);
