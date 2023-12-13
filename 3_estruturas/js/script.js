b6baa734a5ea55227a0a10e6c8ea9384a2626601
Brunoake <126792752+Brunoake@users.noreply.github.com>
2023-12-13 11:13:14 -0300

    variaveis

---
+++ 3_estruturas/js/script.js
@@ -0,0 +1,19 @@
+// variaveis
+
+let nome = "bruninho";
+
+
+console.log(nome);
+
+
+let num = 1;
+
+let num2= 3;
+
+console.log (num + num2);
+
+// mais sobre variaveis
+
+let a = 3, b = 14, c = 20;
+
+console.log(a * c + b);
