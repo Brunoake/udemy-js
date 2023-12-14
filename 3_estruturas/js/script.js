13517e7fddd317d234451b8ab5a1b45f1e4f7198
Brunoake <126792752+Brunoake@users.noreply.github.com>
2023-12-13 23:28:26 -0300

    Update script.js

--- 3_estruturas/js/script.js
+++ 3_estruturas/js/script.js
@@ -17,3 +17,30 @@ console.log (num + num2);
 let a = 3, b = 14, c = 20;
 
 console.log(a * c + b);
+
+// Prompt
+
+//  const age = prompt("qual sua idade");
+
+//  console.log(`Voce tem ${age} anos.`);
+
+// alert
+
+// alert ("Oi mundo")
+
+// const z = 10;
+
+// alert (`Sua idade é ${z}`);
+// math
+
+console.log(Math.max(2, 1, 13, 11));
+console.log(Math.floor(5.14));
+console.log(Math.ceil(5.89));
+
+// console
+  
+console.log("teste");
+
+console.error("erro!");
+
+console.warn("aviso!");
