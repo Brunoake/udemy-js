2937966ca1b4cd9f3f5c2cbac7afd54e8c4ef30d
Brunoake <126792752+Brunoake@users.noreply.github.com>
2023-12-14 15:44:06 -0300

    Update script.js

--- 4_ARQUIVOS/js/script.js
+++ 4_ARQUIVOS/js/script.js
@@ -5,4 +5,63 @@ function minhafuncao() {
 }
 
 
-minhafuncao();
+// retorno de função
+
+const a = 10;
+const b = 20;
+const c = 30;
+const d = 40;
+
+function soma(n1, n2) {
+    return n1 + n2
+}
+
+const resultado = soma(a, b);
+
+console.log(resultado);
+
+console.log(soma(c, d));
+
+// escopo de função
+
+let y = 20;
+
+function testandoescopo() {
+    let y = 10
+    console.log(`y dentro da função é ${y}`);
+}
+
+testandoescopo();
+
+y = 300;
+console.log(`y fora da função é ${y}`);
+
+// escopo aninhado
+
+let n = 10;
+
+function escopoaninhado() {
+
+    let n = 20
+
+    if(true){
+        let n =30;
+      
+    
+    if(true){
+        let n =40;
+        console.log(n);
+    }
+        console.log(n);
+      
+        }
+        
+            console.log(n);
+      
+      }
+    
+
+
+escopoaninhado();
+
+console.log(n);
