9b413f95e11ea96130caf78e955a5bac3e0edd30
Brunoake <126792752+Brunoake@users.noreply.github.com>
2023-12-16 08:55:46 -0300

    .

---
+++ 5_Arrays/js/script.js
@@ -0,0 +1,39 @@
+// array
+
+const lista = [1, 2, 3, 4, 5]
+
+console.log(lista);
+
+
+const items = ["Bruno", 2, "maçÃ", 4]
+
+console.log(items)
+
+// propriedades 
+
+const number = [5, 4, 4]
+
+console.log(number.length)
+
+console.log(number["length"])
+
+const myname = "Bruno"
+
+console.log(myname.length)
+
+// métodos
+
+const othernumbers = [1, 2, 3]
+
+const allnumbers = number.concat(othernumbers)
+
+
+console.log(allnumbers);
+
+const txt = "algum texto"
+
+console.log(txt.toUpperCase())
+
+console.log(typeof txt.toUpperCase)
+
+console.log(txt.indexOf("g"))
