df4263f9aa2c7940f2074b9deba867c71a2a5430
Brunoake <126792752+Brunoake@users.noreply.github.com>
2023-12-16 21:45:35 -0300

    Update script.js

--- 5_Arrays/js/script.js
+++ 5_Arrays/js/script.js
@@ -1,68 +1,108 @@
-// array
+// // array
 
-const lista = [1, 2, 3, 4, 5]
+// const lista = [1, 2, 3, 4, 5]
 
-console.log(lista);
-
-
-const items = ["Bruno", 2, "maçÃ", 4]
-
-console.log(items)
-
-// propriedades 
-
-const number = [5, 4, 4]
-
-console.log(number.length)
-
-console.log(number["length"])
-
-const myname = "Bruno"
-
-console.log(myname.length)
-
-// métodos
-
-const othernumbers = [1, 2, 3]
-
-const allnumbers = number.concat(othernumbers)
+// console.log(lista);
 
 
-console.log(allnumbers);
+// const items = ["Bruno", 2, "maçÃ", 4]
 
-const txt = "algum texto"
+// console.log(items)
 
-console.log(txt.toUpperCase())
+// // propriedades 
 
-console.log(typeof txt.toUpperCase)
+// const number = [5, 4, 4]
 
-console.log(txt.indexOf("g"))
+// console.log(number.length)
 
-// object literals
+// console.log(number["length"])
 
-const person = {
-    name: "Bruno",
-    age: 31,
-    job: "programador"
-}
+// const myname = "Bruno"
 
-console.log(person)
+// console.log(myname.length)
 
-//  novas propriedades
+// // métodos
 
-const car = {
-    engine: 2.0,
-    brand: "vw",
-    km: 20000,
+// const othernumbers = [1, 2, 3]
 
+// const allnumbers = number.concat(othernumbers)
+
+
+// console.log(allnumbers);
+
+// const txt = "algum texto"
+
+// console.log(txt.toUpperCase())
+
+// console.log(typeof txt.toUpperCase)
+
+// console.log(txt.indexOf("g"))
+
+// // object literals
+
+// const person = {
+//     name: "Bruno",
+//     age: 31,
+//     job: "programador"
+// }
+
+// console.log(person)
+
+// //  novas propriedades
+
+// const car = {
+//     engine: 2.0,
+//     brand: "vw",
+//     km: 20000,
+
+// };
+
+// console.log(car)
+
+// car.doors = 4;
+
+// console.log(car)
+
+// delete car.km
+
+// console.log(car)
+
+// // mais sobre objeto
+
+ const obj = {
+     a: "teste",
+     b: true,
+ };
+
+ console.log(obj instanceof Object);
+
+ const obj2 = {
+     c: []
+ }
+
+ Object.assign(obj2, obj)
+
+ console.log(obj2)
+
+//conhecendo melhor os objetos
+
+console.log(Object.keys(obj))
+
+console.log(Object.keys(obj2))
+
+console.log(Object.entries(obj2))
+
+const carr = {
+    motor: 2.0,
+    model: "m2",
+    marca: "bmw",
 };
 
-console.log(car)
+console.log(carr)
 
-car.doors = 4;
+console.log(Object.keys(carr));
 
-console.log(car)
+console.log(Object.entries(carr))
 
-delete car.km
+console.log(typeof carr)
 
-console.log(car)
