e03c7522fc28c8287ac5c8de45a1b6fca8588ceb
Brunoake <126792752+Brunoake@users.noreply.github.com>
2023-12-20 14:28:44 -0300

    .

---
+++ 6_oop/js/script.js
@@ -0,0 +1,36 @@
+//  1 - métodos
+
+const animal = {
+    nome: "BRUno",
+    latir: function(){
+        console.log("au au")
+    }
+};
+
+console.log(animal.nome);
+
+animal.latir();
+
+// aprofundando em objetos
+
+const pessoa = {
+
+    nome: "Bruno",
+
+    getNome: function() {
+
+        return this.nome;
+    },
+
+    setNome: function(novonome) {
+        this.nome = novonome;
+    },
+}
+
+console.log(pessoa.nome)
+
+console.log(pessoa.getNome())
+
+pessoa.setNome("macaico")
+
+console.log(pessoa.getNome())
