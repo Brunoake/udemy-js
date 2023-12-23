40706c2b492991d0b8ea5c18f3b1c7abd6988173
Brunoake <126792752+Brunoake@users.noreply.github.com>
2023-12-23 12:26:04 -0300

    Update script.js

--- 6_oop/js/script.js
+++ 6_oop/js/script.js
@@ -45,3 +45,63 @@ console.log(Object.getPrototypeOf(text));
 const bool = true;
 
 console.log(Object.getPrototypeOf(bool));
+
+// Mais sobre prototype
+
+const  myObject = {
+    a: "b"
+}
+
+console.log(Object.getPrototypeOf(myObject));
+
+console.log(Object.getPrototypeOf(myObject)===Object.prototype)
+
+const mySecondObject = Object.create(myObject);
+
+console.log(mySecondObject);
+
+//  Classes básicas
+
+const cachorro = {
+    raca: null,
+    Patas: 4,
+}
+
+const pastorAlemao = Object.create(cachorro);
+
+pastorAlemao.raca = "pastor Alemão";
+
+console.log(pastorAlemao);
+
+console.log(pastorAlemao.patas);
+
+console.log(pastorAlemao);
+
+const bulldog = Object.create(cachorro)
+
+bulldog.raca = "bulldog"
+
+console.log(bulldog)
+
+// função construtora
+
+function criarCachorro(nome, raca) {
+
+    const cachorro = Object.create({
+    })
+
+    cachorro.nome = nome
+    cachorro.raca =raca
+
+    return cachorro
+}
+
+const bob = criarCachorro("bob", "vir-lata");
+
+console.log(bob);
+
+const jack = criarCachorro("jack", "poodle")
+
+console.log(jack)
+
+console.log(Object.getPrototypeOf(jack))
