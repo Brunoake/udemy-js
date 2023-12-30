c556e4f09c923b5838d96f0c9ff0d6a3d37d2b0f
Brunoake <126792752+Brunoake@users.noreply.github.com>
2023-12-29 22:11:49 -0300

    .

--- 9_DOM/js/script.js
+++ 9_DOM/js/script.js
@@ -9,5 +9,37 @@ console.log(document.body.childNodes[1].childNodes)
 
 console.log(document.body.childNodes[1].childNodes[1].textContent)
 
-// selecionando elementos
+// Selecionando por tag
 
+const listItens = document.getElementsByTagName("li")
+
+console.log(listItens)
+
+// Selecionando por id
+
+const title = document.getElementById("title")
+
+console.log(title)
+
+// Selecionando por classe
+const products = document.getElementsByClassName("product")
+
+console.log(products)
+
+// Selecionado por css
+
+const productsQuery = document.querySelectorAll(".product")
+
+console.log(productsQuery)
+
+const maincontainer = document.querySelector("#main-container")
+
+console.log(maincontainer)
+
+// Alterando html com insertBefore
+
+const p = document.createElement("p");
+
+const header = title.parentElement;
+
+header.insertBefore(p, title);
